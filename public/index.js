f3d5002f26f7b00345a8a0ba5832a86393af5d85
gushags <jeff@hagkull.com>
2025-09-15 14:08:09 -0700

    Feature: finish category selection implementation

---
+++ public/index.js
@@ -0,0 +1,47 @@
+// index.js
+
+function submitCategorySearch() {
+  const collection = document.querySelector(
+    'input[name="collection"]:checked'
+  ).value;
+  const wood = document.querySelector('input[name="wood"]:checked').value;
+  const ftype = document.querySelector('input[name="ftype"]:checked').value;
+  const room = document.querySelector('input[name="room"]:checked').value;
+  const string = collection + '&' + wood + '&' + ftype + '&' + room;
+  localStorage.clear();
+
+  const radio = {
+    collection: collection,
+    wood: wood,
+    ftype: ftype,
+    room: room,
+  };
+  localStorage.setItem('radioButtons', JSON.stringify(radio));
+  window.location.replace(`../${string}`);
+}
+
+function checkRadioButtonByValue(name, valueToSelect) {
+  const radioButtons = document.getElementsByName(name);
+  for (let i = 0; i < radioButtons.length; i++) {
+    if (radioButtons[i].value === valueToSelect) {
+      radioButtons[i].checked = true;
+      break; // Exit the loop once the correct radio button is found and checked
+    }
+  }
+}
+
+function updateRadioButtons() {
+  if (localStorage) {
+    const storedRadio = JSON.parse(localStorage.getItem('radioButtons'));
+    const collectionRad = storedRadio.collection;
+    const woodRad = storedRadio.wood;
+    const ftypeRad = storedRadio.ftype;
+    const roomRad = storedRadio.room;
+    checkRadioButtonByValue('collection', collectionRad);
+    checkRadioButtonByValue('wood', woodRad);
+    checkRadioButtonByValue('ftype', ftypeRad);
+    checkRadioButtonByValue('room', roomRad);
+  }
+}
+
+updateRadioButtons();
