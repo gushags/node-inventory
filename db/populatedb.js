0a4aebe1c59a378aebf0eb87bd1fd6ed676f7882
gushags <jeff@hagkull.com>
2025-10-02 12:10:30 -0700

    DB: change schema to allow tagging of inventory items

--- db/populatedb.js
+++ db/populatedb.js
@@ -7,6 +7,8 @@ const { PGPOSTGRESQLURL } = process.env;
 
 const SQL = `
 DROP TABLE IF EXISTS furniture_inventory;
+DROP TABLE IF EXISTS inventory_rooms;
+DROP TABLE IF EXISTS inventory_types;
 DROP TABLE IF EXISTS furniture;
 DROP TABLE IF EXISTS wood;
 DROP TABLE IF EXISTS collections;
@@ -55,35 +57,30 @@ CREATE TABLE furniture (
 ALTER SEQUENCE furniture_furn_id_seq RESTART WITH 500;
 
 CREATE TABLE furniture_inventory (
-  finv_id SERIAL,
+  finv_id SERIAL PRIMARY KEY,
+  furn_id INT NOT NULL REFERENCES furniture(furn_id) ON DELETE CASCADE,
+  wood_id INT REFERENCES wood(wood_id) ON DELETE SET NULL,
+  collection_id INT REFERENCES collections(collection_id) ON DELETE SET NULL,
   finv_quantity INT DEFAULT 0,
-  furn_id INT,
-  wood_id INT,
-  ftype_id INT,
-  room_id INT,
   finv_sku TEXT GENERATED ALWAYS AS (
     (furn_id::TEXT || wood_id::TEXT || finv_id::TEXT)
-  ) STORED,
-  PRIMARY KEY(finv_id),
-  CONSTRAINT fk_furniture
-      FOREIGN KEY(furn_id)
-        REFERENCES furniture(furn_id)
-            ON DELETE CASCADE,
-  CONSTRAINT fk_wood
-      FOREIGN KEY(wood_id)
-        REFERENCES wood(wood_id)
-            ON DELETE SET NULL,
-  CONSTRAINT fk_ftypes
-      FOREIGN KEY(ftype_id)
-        REFERENCES furniture_types(ftype_id)
-            ON DELETE SET NULL,
-  CONSTRAINT fk_rooms
-      FOREIGN KEY(room_id)
-        REFERENCES rooms(room_id)
-            ON DELETE SET NULL
-  CONSTRAINT unique_variant UNIQUE (furn_id, wood_id)
+    ) STORED,
+  CONSTRAINT unique_inventory UNIQUE (furn_id, wood_id, collection_id)
 );
 ALTER SEQUENCE furniture_inventory_finv_id_seq RESTART WITH 1000;
+
+CREATE TABLE inventory_rooms (
+  furn_id INT REFERENCES furniture(furn_id) ON DELETE CASCADE,
+  room_id INT REFERENCES rooms(room_id) ON DELETE CASCADE,
+  PRIMARY KEY (furn_id, room_id)
+);
+
+CREATE TABLE inventory_types (
+  furn_id INT REFERENCES furniture(furn_id) ON DELETE CASCADE,
+  ftype_id INT REFERENCES furniture_types(ftype_id) ON DELETE CASCADE,
+  PRIMARY KEY (furn_id, ftype_id)
+);
+
 
 INSERT INTO wood (wood_name)
 VALUES
@@ -144,95 +141,142 @@ VALUES
     ('Le Mot Juste Table', 'Inmensae subtilitatis, obscuris et malesuada fames.', 200)
     ;
 
-INSERT INTO furniture_inventory (finv_quantity, furn_id, wood_id, ftype_id, room_id)
+INSERT INTO furniture_inventory (finv_quantity, furn_id, wood_id, collection_id)
   VALUES
       --Hemingway
       --chair
-      (30, 500, 100, 303, 401),
-      (40, 500, 101, 303, 401),
-      (40, 500, 102, 303, 401),
-      (40, 500, 103, 303, 401),
+      (30, 500, 100, 202),
+      (40, 500, 101, 202),
+      (40, 500, 102, 202),
+      (40, 500, 103, 202),
       --table
-      (5, 501, 100, 308, 401),
-      (7, 501, 101, 308, 401),
-      (4, 501, 102, 308, 401),
-      (3, 501, 103, 308, 401),
+      (5, 501, 100, 202),
+      (7, 501, 101, 202),
+      (4, 501, 102, 202),
+      (3, 501, 103, 202),
       --hutch
-      (2, 502, 100, 307, 401),
-      (3, 502, 101, 307, 401),
-      (0, 502, 102, 307, 401),
-      (1, 502, 103, 307, 401),
+      (2, 502, 100, 202),
+      (3, 502, 101, 202),
+      (0, 502, 102, 202),
+      (1, 502, 103, 202),
       --deckchair
-      (12, 503, 102, 302, 403),
-      (11, 503, 103, 302, 403),
+      (12, 503, 102, 202),
+      (11, 503, 103, 202),
       --bench
-      (8, 504, 100, 301, 402),
-      (5, 504, 101, 301, 402),
-      (3, 504, 102, 301, 402),
-      (17, 504, 103, 301, 402),
+      (8, 504, 100, 202),
+      (5, 504, 101, 202),
+      (3, 504, 102, 202),
+      (17, 504, 103, 202),
       --bed
-      (2, 505, 100, 300, 400),
-      (1, 505, 101, 300, 400),
-      (2, 505, 102, 300, 400),
-      (2, 505, 103, 300, 400),
+      (2, 505, 100, 202),
+      (1, 505, 101, 202),
+      (2, 505, 102, 202),
+      (2, 505, 103, 202),
       --headboard
-      (2, 506, 100, 306, 400),
-      (1, 506, 101, 306, 400),
-      (2, 506, 102, 306, 400),
-      (2, 506, 103, 306, 400),
+      (2, 506, 100, 202),
+      (1, 506, 101, 202),
+      (2, 506, 102, 202),
+      (2, 506, 103, 202),
       --side table
-      (8, 507, 100, 309, 402),
-      (6, 507, 101, 309, 402),
-      (7, 507, 102, 309, 402),
-      (0, 507, 103, 309, 402),
+      (8, 507, 100, 202),
+      (6, 507, 101, 202),
+      (7, 507, 102, 202),
+      (0, 507, 103, 202),
       --dresser
-      (2, 508, 100, 304, 400),
-      (0, 508, 101, 304, 400),
-      (1, 508, 102, 304, 400),
-      (1, 508, 103, 304, 400),
+      (2, 508, 100, 202),
+      (0, 508, 101, 202),
+      (1, 508, 102, 202),
+      (1, 508, 103, 202),
       --end table
-      (4, 509, 100, 305, 400),
-      (7, 509, 101, 305, 400),
-      (2, 509, 102, 305, 400),
-      (2, 509, 103, 305, 400),
+      (4, 509, 100, 202),
+      (7, 509, 101, 202),
+      (2, 509, 102, 202),
+      (2, 509, 103, 202),
       --Fitzgerald
       --deckchair
-      (5, 510, 102, 302, 403),
-      (2, 510, 103, 302, 403),
+      (5, 510, 102, 201),
+      (2, 510, 103, 201),
       --chair
-      (20, 511, 100, 303, 401),
-      (41, 511, 101, 303, 401),
-      (34, 511, 102, 303, 401),
-      (20, 511, 103, 303, 401),
+      (20, 511, 100, 201),
+      (41, 511, 101, 201),
+      (34, 511, 102, 201),
+      (20, 511, 103, 201),
       --table
-      (2, 512, 100, 308, 401),
-      (8, 512, 101, 308, 401),
-      (3, 512, 102, 308, 401),
-      (1, 512, 103, 308, 401),
+      (2, 512, 100, 201),
+      (8, 512, 101, 201),
+      (3, 512, 102, 201),
+      (1, 512, 103, 201),
       --dresser
-      (2, 513, 100, 304, 400),
-      (3, 513, 101, 304, 400),
-      (4, 513, 102, 304, 400),
-      (1, 513, 103, 304, 400),
+      (2, 513, 100, 201),
+      (3, 513, 101, 201),
+      (4, 513, 102, 201),
+      (1, 513, 103, 201),
       --end table
-      (7, 514, 100, 305, 400),
-      (9, 514, 101, 305, 400),
-      (6, 514, 102, 305, 400),
-      (8, 514, 103, 305, 400),
+      (7, 514, 100, 201),
+      (9, 514, 101, 201),
+      (6, 514, 102, 201),
+      (8, 514, 103, 201),
       --bench
-      (12, 515, 100, 301, 402),
-      (10, 515, 101, 301, 402),
-      (22, 515, 102, 301, 402),
-      (17, 515, 103, 301, 402),
+      (12, 515, 100, 201),
+      (10, 515, 101, 201),
+      (22, 515, 102, 201),
+      (17, 515, 103, 201),
       --Ezra Collection
       --chair
-      (32, 516, 101, 303, 401),
-      (40, 516, 102, 303, 401),
-      (22, 516, 103, 303, 401),
+      (32, 516, 101, 200),
+      (40, 516, 102, 200),
+      (22, 516, 103, 200),
       --table
-      (2, 517, 101, 308, 401),
-      (9, 517, 102, 308, 401),
-      (3, 517, 103, 308, 401);
+      (2, 517, 101, 200),
+      (9, 517, 102, 200),
+      (3, 517, 103, 200);
+
+INSERT INTO inventory_rooms (furn_id, room_id)
+  VALUES
+      (500, 401),
+      (500, 402),
+      (501, 401),
+      (502, 401),
+      (503, 403),
+      (504, 401),
+      (504, 402),
+      (505, 400),
+      (506, 400),
+      (507, 400),
+      (507, 402),
+      (508, 400),
+      (509, 400),
+      (510, 403),
+      (511, 401),
+      (511, 402),
+      (512, 401),
+      (513, 400),
+      (514, 400),
+      (515, 401),
+      (515, 402),
+      (516, 401),
+      (516, 402),
+      (517, 401);
+
+INSERT INTO inventory_types (furn_id, ftype_id)
+  VALUES
+      (500, 303),
+      (501, 308),
+      (503, 302),
+      (504, 301),
+      (505, 300),
+      (506, 306),
+      (507, 309),
+      (508, 304),
+      (509, 305),
+      (510, 302),
+      (511, 303),
+      (512, 308),
+      (513, 304),
+      (514, 305),
+      (515, 301),
+      (516, 303),
+      (517, 308);
     `;
 
 async function main() {
