58cfe6c573aaaa84d0a974298770267a53287205
gushags <jeff@hagkull.com>
2025-10-03 10:54:21 -0700

    CRUD: validate fields before new product submission

--- public/new.js
+++ public/new.js
@@ -13,3 +13,60 @@ for (const w of woodQty) {
     }
   });
 }
+
+// Form validation -- ensure checkboxes and quantity are entered
+
+document
+  .querySelector('#new-product')
+  .addEventListener('submit', function (event) {
+    const woodCheckboxes = document.querySelectorAll('input[name="woodIds[]"]');
+    const roomsCheckboxes = document.querySelectorAll('input[name="roomIds[]');
+    const qtyInputs = document.querySelectorAll('input[name="quantityCnt[]"]');
+
+    const woodValidate = document.getElementById('wood-validation');
+    const roomsValidate = document.getElementById('rooms-validation');
+    const qtyValidate = document.getElementById('qty-validation');
+
+    woodValidate.textContent = '';
+    qtyValidate.textContent = '';
+    roomsValidate.textContent = '';
+
+    let isWoodChecked = false;
+    let isRoomsChecked = false;
+    let isQty = true;
+
+    for (let i = 0; i < woodCheckboxes.length; i++) {
+      if (woodCheckboxes[i].checked) {
+        isWoodChecked = true;
+        break;
+      }
+    }
+    for (let i = 0; i < roomsCheckboxes.length; i++) {
+      if (roomsCheckboxes[i].checked) {
+        isRoomsChecked = true;
+        break;
+      }
+    }
+    for (let i = 0; i < qtyInputs.length; i++) {
+      if (qtyInputs[i].value === '' && !qtyInputs[i].disabled) {
+        isQty = false;
+        break;
+      }
+    }
+
+    if (!isWoodChecked) {
+      event.preventDefault(); // Prevent form submission
+      woodValidate.textContent = 'Please select at least one wood option.';
+    } else if (!isQty) {
+      event.preventDefault();
+      qtyValidate.textContent =
+        'Please select quantities for each wood option.';
+    } else if (!isRoomsChecked) {
+      event.preventDefault();
+      roomsValidate.textContent = 'Please select at least one room option.';
+    } else {
+      woodValidate.textContent = '';
+      qtyValidate.textContent = '';
+      roomsValidate.textContent = '';
+    }
+  });
